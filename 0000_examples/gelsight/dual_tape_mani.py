b93db8be8610226a4adbbe3158a97518b3dda3e3
Photon26 <hjcz_gaoyuan@126.com>
2021-06-18 06:35:10 +0900

    6.18

--- 0000_examples/gelsight/dual_tape_mani.py
+++ 0000_examples/gelsight/dual_tape_mani.py
@@ -6,6 +6,7 @@ import modeling.geometric_model as gm
 import numpy as np
 import cv2
 import img_to_depth as itd
+import time
 
 video1 = cv2.VideoCapture(0)
 itd_cvter = itd.ImageToDepth()
@@ -85,7 +86,7 @@ inner_rad = 75/2
 outer_rad = 99/2
 
 robot_instance = ur3d.UR3Dual()
-# ur_dual_x = ur3dx.UR3DualX(lft_robot_ip='10.2.0.50', rgt_robot_ip='10.2.0.51', pc_ip='10.2.0.100')
+ur_dual_x = ur3dx.UR3DualX(lft_robot_ip='10.2.0.50', rgt_robot_ip='10.2.0.51', pc_ip='10.2.0.100')
 
 base = wd.World(cam_pos=[2, 1, 3], lookat_pos=[0, 0, 1.1])
 gm.gen_frame().attach_to(base)
@@ -118,16 +119,19 @@ for theta in range(3,15):
     jnt_list.append(newjnt_rgt)
     pre_jnt = newjnt_rgt
 
+
 # rgt hand hold the tape
 # jnt_rgt = robot_s.ik("rgt_arm", ini_pos, ini_rot_rgt)
 ini_jnt_rgt = jnt_list[0]
-# ur_dual_x.rgt_arm_hnd.move_jnts(ini_jnt_rgt)
+ur_dual_x.rgt_arm_hnd.move_jnts(ini_jnt_rgt)
 
 #  loose lft hand a bit
-ini_pos_lft = ini_pos + np.dot(ini_rot_lft, np.array([0,0.005,0]))
+# ini_pos_lft = ini_pos + np.dot(ini_rot_lft, np.array([0,0.005,0]))
 newjnt = robot_s.ik("lft_arm",ini_pos, ini_rot_lft, max_niter=1000)
 robot_s.fk("lft_arm", newjnt)
-# ur_dual_x.lft_arm_hnd.move_jnts(newjnt)
+ur_dual_x.lft_arm_hnd.move_jnts(newjnt)
+ur_dual_x.lft_arm_hnd.close_gripper(speedpercentange=20, forcepercentage=0) #   gripper control
+
 robot_meshmodel = robot_s.gen_meshmodel(toggle_tcpcs=False)
 robot_meshmodel.attach_to(base)
 
@@ -135,9 +139,35 @@ robot_meshmodel.attach_to(base)
 for jnt in jnt_list:
     if jnt is not None:
         print("*")
-        # ur_dual_x.rgt_arm_hnd.move_jnts(jnt)
+        ur_dual_x.rgt_arm_hnd.move_jnts(jnt)
         robot_s.fk("rgt_arm", jnt)
         robot_meshmodel = robot_s.gen_meshmodel(toggle_tcpcs=False)
         robot_meshmodel.attach_to(base)
+
+        #   detect the tape edge
+        dur = 0
+        theta = 0
+        # time.sleep(10)
+        count = 0
+        tic = time.time()
+        flag = 0  # 1 if edge is detected, 0 if no edge is detected.
+        while (dur < 1):
+            return_value, image = video1.read()
+            depth, hm = itd_cvter.convert(image)
+            dz, theta1 = hm2pos(hm)
+            print(theta1)
+            if theta1 is not None:
+                if np.abs(theta1 - theta) < 3 / 180 * np.pi:
+                    count = count + 1
+                else:
+                    count = 0
+                theta = theta1
+                if count == 10:
+                    flag = 1
+                    break
+            dur = time.time() - tic
+
+        if flag == 1:
+            break
 
 base.run()
